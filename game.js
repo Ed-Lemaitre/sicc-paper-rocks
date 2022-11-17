662e12c1c3df25011b29e04f0eccf272dfc6489a
Ed-Lemaitre <97646941+Ed-Lemaitre@users.noreply.github.com>
2022-11-17 08:53:45 -0300

    finished js code

--- game.js
+++ game.js
@@ -1,13 +1,11 @@
 let computerPoints = 0, playerPoints = 0
 
-function getComputerChoice() {
-    let computerPlay = ['s','p','r']
-    return computerPlay[Math.floor(Math.random()*3)]
+function getComputerChoice() {    //Computer selects a random choice
+    let computerPlay = ['s', 'p', 'r']
+    return computerPlay[Math.floor(Math.random() * 3)]
 }
 
-
-
-function playRound(computerSelection, playerSelection){
+function playRound(computerSelection, playerSelection) {     // This func with define winner 
     if (computerSelection == playerSelection) {
         return "It's a Tie"
     } else {
@@ -15,53 +13,56 @@ function playRound(computerSelection, playerSelection){
     }
 }
 
-function defineWinner(computerSelection,playerSelection){
+function defineWinner(computerSelection, playerSelection) {
     if (computerSelection == "s") {
         if (playerSelection == "p") {
-            computerPoints = computerPoints + 1
-            return computerPoints
+            computerPoints = computerPoints + 1;
+            return computerPoints;
         } else {
-            playerPoints = playerPoints + 1
-            return playerPoints
+            playerPoints = playerPoints + 1;
+            return playerPoints;
         }
     }
     if (computerSelection == "p") {
         if (playerSelection == "r") {
-            computerPoints = computerPoints + 1
-            return computerPoints
+            computerPoints = computerPoints + 1;
+            return computerPoints;
         } else {
-            playerPoints = playerPoints + 1
-            return playerPoints
+            playerPoints = playerPoints + 1;
+            return playerPoints;
         }
     }
     if (computerSelection == "r") {
         if (playerSelection == "s") {
-            computerPoints = computerPoints + 1
-            return computerPoints
+            computerPoints = computerPoints + 1;
+            return computerPoints;
         } else {
-            playerPoints = playerPoints + 1
-            return playerPoints
+            playerPoints = playerPoints + 1;
+            return playerPoints;
         }
     }
 }
 
 function playingGame() {
-    for (let i = 0; i<5;i++){
-        computerSelection = getComputerChoice()
-        playerSelection = prompt("Enter your selection: ").toLowerCase()[0]
-        resultadoRonda = playRound(computerSelection,playerSelection)
-        if (resultadoRonda == "It's a Tie"){
-            console.log("Empataron!!!. Jueguen nuevamente")
-            i = i -1
-        } else { 
-            console.log("Tu puntaje: " + playerPoints)
-            console.log("El Puntaje del PC: " + computerPoints)
+    for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Enter your selection: ").toLowerCase()[0];
+        console.log("El PC jugó: " + computerSelection + ". Tu jugaste: " + playerSelection);
+        resultadoRonda = playRound(computerSelection, playerSelection);
+        if (resultadoRonda == "It's a Tie") {
+            console.log("Empataron!!!. Juegen otra Ronda");
+            i = i - 1;
+            console.log("Tu puntaje: " + playerPoints);
+            console.log("El Puntaje del PC: " + computerPoints);
+        } else {
+            console.log("Tu puntaje: " + playerPoints);
+            console.log("El Puntaje del PC: " + computerPoints);
         }
     }
     if (playerPoints > computerPoints) {
-        console.log("Congratulations!!!! You WIN!!!")
-    } else{
-        console.log("Loser!!!")
+        console.log("Congratulations!!!! You WIN!!!");
+    } else {
+        console.log("Loser!!!");
     }
 
 
